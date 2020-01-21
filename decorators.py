45322efd541ad76e6aee36ad783fc0839f249477
Emilio Araos <ea3dev@gmail.com>
2020-01-20 22:01:19 -0700

    final method

--- decorators.py
+++ decorators.py
@@ -43,8 +43,28 @@ def hello(name='Emilio'):
 my_new_func = hello("Emilio")
 print(my_new_func())
 
+def new_decorator(original_func):
 
+	def wrap_func():
 
+		print("Extra code, before the original function")
+
+		original_func()
+
+		print("Some extra code, after the original function")
+
+	return wrap_func
+
+def func_needs_decorator():
+	print("I want to be decorated")
+
+decorated_function = new_decorator(func_needs_decorator)
+decorated_function()
+
+@new_decorator
+def func_needs_decorator():
+	print("I want to be decorated")
+func_needs_decorator()
 
 
 
